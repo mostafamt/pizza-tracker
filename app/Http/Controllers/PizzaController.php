3a4d30aafb617813ce43c01dcf1f47c6791d1623
mostafa <mostafa.kamel@almotahidaeducation.com>
2024-03-04 17:56:25 +0200

    first commit

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pizza;
+use Inertia\Inertia;
+
+class PizzaController extends Controller
+{
+    //
+    public function index()
+    {
+        $pizzas = Pizza::all();
+        return Inertia::render('Pizzas/All', [
+            'pizzas' => $pizzas
+        ]);
+    }
+}
